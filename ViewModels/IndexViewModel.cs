09bce283d4fb5177b7a2a84dd1ab1ed82cf8f4bd
Jamie Fraser <jfraser@newsignature.com>
2021-03-10 07:50:12 -0500

    Removed dead code

--- ViewModels/IndexViewModel.cs
+++ ViewModels/IndexViewModel.cs
@@ -14,25 +14,9 @@ namespace FunWithReflection.ViewModels
         {
 
             services = _services;
-            //foreach (var s in services)
-            //{
-            //    Console.WriteLine($"There is a registered service of type with a servicetype of {s.ServiceType.Name}");
-            //}
             foreach(var x in typeof(App).Assembly.CreatableTypes().EndingWith("Service"))
             {
                 services.GetServiceInstanceAndExecuteMethod(provider, x.GetInterfaces()[0], "Refresh");
-                //Console.WriteLine($"working with a type called {x.GetInterfaces()[0].Name}");
-                //var s = services.Where(t => t.ServiceType.Equals(x.GetInterfaces()[0])).FirstOrDefault();
-                //if(s != null)
-                //{
-
-                //    Console.WriteLine($"Found a type with the name {x.Name} - trying to do something with it");
-                //    dynamic implementation = provider.GetRequiredService(s.ServiceType);
-                //    Console.WriteLine($"The ImplementationType is {s.ImplementationType.Name}");
-                //    implementation.Refresh();
-                //    //dynamic svc = s;
-                //    //svc.Refresh();
-                //}
             }
         }
     }
